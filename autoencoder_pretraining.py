9d63bf886401b1b030bb12e4e0e5c5978117f3f2
kalvik <kd.kalvik@gmail.com>
2018-03-16 23:03:35 -0500

    added data generators

--- autoencoder_pretraining.py
+++ autoencoder_pretraining.py
@@ -1,10 +1,13 @@
 from keras.layers import Input, Conv1D, UpSampling1D, MaxPooling1D
-from keras.models import Model
-import numpy as np
-from keras import optimizers
 from keras.callbacks import TensorBoard, ModelCheckpoint
-import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.core import Lambda
 from sklearn.utils import shuffle
+from keras.models import Model
+from keras import backend as K
+from keras import optimizers
+import pandas as pd
+import numpy as np
 import time
 import h5py
 
@@ -38,14 +41,10 @@ print('importing dataset')
 
 with h5py.File(srcdir + '/data.h5', 'r') as hf:
     train_x = hf['train_x'][:]
-    train_y = hf['train_y'][:]
-    test_x = hf['test_x'][:]
-    test_y = hf['test_y'][:]
     val_x = hf['val_x'][:]
-    val_y = hf['val_y'][:]
 
 
-print(train_x.shape, train_y.shape, test_x.shape, test_y.shape, val_x.shape, val_y.shape)
+print(train_x.shape, val_x.shape)
 
 #############################################################################################
 
@@ -86,49 +85,55 @@ x_data_phase = scale_big_data(x_data_phase, minn=minel_phase, maxx=maxel_phase)
 
 val_x = np.concatenate((x_data_mag, x_data_phase), axis=-1)
 
-print("Final preprocessed data shape: ", train_x.shape, train_y.shape, val_x.shape, val_y.shape)
+train_x = np.expand_dims(train_x, -1)
+val_x = np.expand_dims(val_x, -1)
 
-# #############################################################################################
+del x_data_mag, x_data_phase
 
-def get_csi_minibatch(train_x, train_y, batch_size):
-    while True:
-        train_x, train_y = shuffle(train_x, train_y)
-        for p in range(0, train_x.shape[0], batch_size):
-            yy_data = train_x[p: p + batch_size]
-            yy_label = train_y[p: p + batch_size]
-            yield yy_data, yy_label
+print("Final preprocessed data shape: ", train_x.shape, val_x.shape)
 
 # ##########################################################################################
+def expand_dims(x):
+    return K.expand_dims(x, -1)
+    
+def squeeze(x):
+    return K.squeeze(x, -1)
+
 
 print('creating network')
-inputs = Input(shape=(win_size, num_cols))
+inputs = Input(shape=(win_size, num_cols, 1))
 
-x = Conv1D(256, 200, padding="same", name="encoder_Conv1")(inputs)
+x = Lambda(squeeze)(inputs)
+
+x = Conv1D(256, 200, padding="same", name="encoder_Conv1", activation="relu")(x)
 x = MaxPooling1D(2, strides = 2, name= "encoder_max1")(x)
 
-x = Conv1D(128, 200, padding="same", name="encoder_Conv2")(x)
+x = Conv1D(128, 200, padding="same", name="encoder_Conv2", activation="relu")(x)
 x = MaxPooling1D(2, strides = 2, name= "encoder_max2")(x)
 
-x = Conv1D(64, 200, padding="same", name="encoder_Conv3")(x)
+x = Conv1D(64, 200, padding="same", name="encoder_Conv3", activation="relu")(x)
 x = MaxPooling1D(2, strides = 2, name= "encoder_max3")(x)
 
-x = Conv1D(32, 200, padding="same", name="encoder_Conv4")(x)
+x = Conv1D(32, 200, padding="same", name="encoder_Conv4", activation="relu")(x)
+
 encoder = MaxPooling1D(2, strides = 2, name= "encoder_max4")(x)
 
 
-x = Conv1D(32, 200, padding="same", name="decoder_Conv1")(encoder)
+x = Conv1D(32, 200, padding="same", name="decoder_Conv1", activation="relu")(encoder)
 x = UpSampling1D(size = 2, name="decoder_up1")(x)
 
-x = Conv1D(64, 200, padding="same", name="decoder_Conv2")(x)
+x = Conv1D(64, 200, padding="same", name="decoder_Conv2", activation="relu")(x)
 x = UpSampling1D(size = 2, name="decoder_up2")(x)
 
-x = Conv1D(128, 200, padding="same", name="decoder_Conv3")(x)
+x = Conv1D(128, 200, padding="same", name="decoder_Conv3", activation="relu")(x)
 x = UpSampling1D(size = 2, name="decoder_up3")(x)
 
-x = Conv1D(256, 200, padding="same", name="decoder_Conv4")(x)
+x = Conv1D(256, 200, padding="same", name="decoder_Conv4", activation="relu")(x)
 x = UpSampling1D(size = 2, name="decoder_up4")(x)
 
-x = Conv1D(num_cols, 200, padding="same", name="reshape_conv")(x)
+x = Conv1D(num_cols, 200, padding="same", name="reshape_conv", activation="tanh")(x)
+
+x = Lambda(expand_dims)(x)
 
 model = Model(inputs, x)
 model.summary()
@@ -137,23 +142,20 @@ model.compile(optimizer=optimizers.Nadam(),
               loss='mean_squared_error',
               metrics=['accuracy'])
 
-train = get_csi_minibatch(train_x, train_x, batch_size=batch_size)
-validation = get_csi_minibatch(val_x, val_x, batch_size=batch_size)
+train_data = ImageDataGenerator()
+val_data = ImageDataGenerator()
 
-tensorboard = TensorBoard(log_dir='./logs/autoencoder_weights_{0}'.format(time.time()),
-                 histogram_freq=0,
-                 write_graph=True,
-                 write_images=False)
+tensorboard = TensorBoard(log_dir='./logs/autoencoder_weights_{0}'.format(time.time()), write_graph=True)
 
 filepath="./weights/autoencoder_weights_{epoch:02d}_{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max', period=5)
 
 print('training started')
 
-model.fit_generator(train,
+model.fit_generator(train_data.flow(train_x, train_x, batch_size=batch_size),
                     steps_per_epoch = train_x.shape[0]//batch_size,
                     epochs = EPOCHS,
                     callbacks = [tensorboard, checkpoint],
-                    validation_data = validation,
+                    validation_data = val_data.flow(val_x, val_x, batch_size=batch_size),
                     validation_steps = val_x.shape[0]//batch_size,
                     verbose = 1)
