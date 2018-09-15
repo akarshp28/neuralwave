249a004e57cf4718b5ec1d19d67123bf05145495
Kalvik <kalvik>
2018-09-15 18:09:12 -0400

    added script to use preprocessed scalers to process new data

---
+++ preprocess/apply_preprocess_l2.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python3
+
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from scipy.io import loadmat
+import numpy as np
+import argparse
+import pickle
+import time
+import h5py
+import math
+import sys
+import os
+
+def get_csi(x):
+    x = np.squeeze(x["csi_trace"])
+    data = []
+    for i in range(x.shape[0]):
+        array = {}
+        array["timestamp_low"] = np.squeeze(x[i][0][0][0])
+        array["bfee_count"] =  np.squeeze(x[i][0][0][1])
+        array["Nrx"] = np.squeeze(x[i][0][0][2])
+        array["Ntx"] = np.squeeze(x[i][0][0][3])
+        array["rssi_a"] = np.squeeze(x[i][0][0][4])
+        array["rssi_b"] = np.squeeze(x[i][0][0][5])
+        array["rssi_c"] = np.squeeze(x[i][0][0][6])
+        array["noise"] = np.squeeze(x[i][0][0][7])
+        array["agc"] = np.squeeze(x[i][0][0][8])
+        array["perm"] = np.squeeze(x[i][0][0][9])
+        array["rate"] = np.squeeze(x[i][0][0][10])
+        array["csi"] = np.squeeze(x[i][0][0][11])
+        
+        data.append(array)
+    return data
+
+def phase_correction(ph_raw):
+    m = np.arange(-28,29)
+    Tp = np.unwrap(ph_raw)
+    k_param = (Tp[29] - Tp[0])/(m[29] - m[0]);
+    b_param = np.sum(Tp)*(1/30)
+
+    correct_phase = []
+    for i in range(30):
+        correct_phase.append(Tp[i] - k_param*m[i] - b_param)
+    return correct_phase
+
+# 3 x 3 MIMO Matrix format
+# [h11 h12 h13
+# h21 h22 h23
+# h31 h32 h33]
+def apply_phcorrect(ph_raw):
+    mimo_mat = np.rollaxis(ph_raw, 2, 0)
+    mimo_mat = np.reshape(mimo_mat, (30, 9))
+
+    crct_ph = []
+    for col in range(9):
+        crct_ph.append(phase_correction(np.array(mimo_mat)[:, col]))
+
+    stack_crc_ph = np.vstack(crct_ph).T
+
+    restore_ph_mat = []
+    for i in range(30):
+        restore_ph_mat.append(stack_crc_ph[i, :].reshape((3,3)))
+    return np.array(restore_ph_mat).T
+
+def fill_gaps(csi_trace, technique):
+    amp_data = []
+    ph_data = []
+
+    for ind in range(len(csi_trace)):
+        csi_entry = csi_trace[ind]
+
+        scaled_csi = get_scaled_csi(csi_entry)
+        amp = np.absolute(scaled_csi)
+        ph = np.angle(scaled_csi)
+
+        amp_temp=[]
+        ph_temp=[]
+
+        if technique == 'fill':
+            if csi_trace[ind]['Ntx'] == 1:
+                ph = np.expand_dims(ph, axis=0)
+                amp = np.expand_dims(amp, axis=0)
+                for i in range(30):
+                    amp_temp.append(np.append(amp[:,:,i], np.zeros((2,3)) + np.nan).reshape((3,3)))
+                    ph_temp.append(np.append(ph[:,:,i], np.zeros((2,3)) + np.nan).reshape((3,3)))
+                amp_data.append(np.array(amp_temp).flatten())
+                ph_data.append(apply_phcorrect(ph_temp).flatten())
+
+            elif csi_trace[ind]['Ntx'] == 2:
+                for i in range(30):
+                    amp_temp.append(np.append(amp[:,:,i], np.zeros((1,3)) + np.nan).reshape((3,3)))
+                    ph_temp.append(np.append(ph[:,:,i], np.zeros((1,3)) + np.nan).reshape((3,3)))
+                amp_data.append(np.array(amp_temp).flatten())
+                ph_data.append(apply_phcorrect(ph_temp).flatten())
+
+            elif csi_trace[ind]['Ntx'] == 3:
+                amp_data.append(np.array(amp).T.flatten())
+                ph_data.append(apply_phcorrect(ph).T.flatten())
+
+        elif technique == 'mean':
+            if csi_trace[ind]['Ntx'] == 1:
+                ph = np.expand_dims(ph, axis=0)
+                amp = np.expand_dims(amp, axis=0)
+
+                mean_amp = np.mean(amp)
+                mean_ph = np.mean(ph)
+
+                for i in range(30):
+                    amp_temp.append(np.append(amp[:,:,i], np.zeros((2,3)) + mean_amp).reshape((3,3)))
+                    ph_temp.append(np.append(ph[:,:,i], np.zeros((2,3)) + mean_ph).reshape((3,3)))
+                ph_temp = np.array(ph_temp).T
+                amp_data.append(np.array(amp_temp).flatten())
+                ph_data.append(apply_phcorrect(ph_temp).flatten())
+
+            elif csi_trace[ind]['Ntx'] == 2:
+                mean_amp = np.mean(amp)
+                mean_ph = np.mean(ph)
+                for i in range(30):
+                    amp_temp.append(np.append(amp[:,:,i], np.zeros((1,3)) + mean_amp).reshape((3,3)))
+                    ph_temp.append(np.append(ph[:,:,i], np.zeros((1,3)) + mean_ph).reshape((3,3)))
+                ph_temp = np.array(ph_temp).T
+                amp_data.append(np.array(amp_temp).flatten())
+                ph_data.append(apply_phcorrect(ph_temp).flatten())
+
+            elif csi_trace[ind]['Ntx'] == 3:
+                amp_data.append(np.array(amp).T.flatten())
+                ph_data.append(apply_phcorrect(ph).T.flatten())
+
+    return np.hstack([amp_data, ph_data])
+
+def dbinv(x):
+    return np.power(10, (np.array(x)/10))
+
+def get_total_rss(csi_st):
+    rssi_mag = 0;
+    if csi_st['rssi_a'] != 0:
+        rssi_mag = rssi_mag + dbinv(csi_st['rssi_a'])
+
+    if csi_st['rssi_b'] != 0:
+        rssi_mag = rssi_mag + dbinv(csi_st['rssi_b'])
+
+    if csi_st['rssi_c'] != 0:
+        rssi_mag = rssi_mag + dbinv(csi_st['rssi_c'])
+
+    return 10*np.log10(rssi_mag) - 44 - csi_st['agc']
+
+def get_scaled_csi(csi_st):
+    csi = csi_st['csi']
+
+    csi_sq = np.multiply(csi, np.conj(csi))
+    csi_pwr = np.sum(csi_sq[:])
+    rssi_pwr = dbinv(get_total_rss(csi_st))
+
+    scale = rssi_pwr / (csi_pwr / 30)
+
+    if (csi_st['noise'] == -127):
+        noise_db = -92
+    else:
+        noise_db = csi_st['noise']
+
+    thermal_noise_pwr = dbinv(noise_db)
+    quant_error_pwr = scale * (csi_st['Nrx'] * csi_st['Ntx'])
+    total_noise_pwr = thermal_noise_pwr + quant_error_pwr;
+
+    ret = csi * np.sqrt(scale / total_noise_pwr);
+    if csi_st['Ntx'] == 2:
+        ret = ret * np.sqrt(2);
+    elif csi_st['Ntx'] == 3:
+        ret = ret * np.sqrt(dbinv(4.5));
+
+    return ret
+
+def read_samples(dataset_path, endswith=".csv"):
+    datapaths, labels = list(), list()
+    label = 0
+    classes = sorted(os.walk(dataset_path).__next__()[1])
+
+    # List each sub-directory (the classes)
+    for c in classes:
+        c_dir = os.path.join(dataset_path, c)
+        walk = os.walk(c_dir).__next__()
+        # Add each image to the training set
+        for sample in walk[2]:
+            # Only keeps csv samples
+            if sample.endswith(endswith):
+                datapaths.append(os.path.join(c_dir, sample))
+                labels.append(label)
+        label += 1
+
+    return datapaths, labels
+
+def compute_data(file_path):
+    if (not os.path.isfile(file_path)):
+        raise ValueError("File dosn't exits")
+
+    csi_trace = get_csi(loadmat(file_path))[2000:10000]
+    csi_trace = fill_gaps(csi_trace, technique='mean')
+
+    return csi_trace.astype(np.float32)
+
+def smooth(x,window_len):
+    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
+    w=np.hanning(window_len)
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    return y[(window_len//2):-(window_len//2)]
+
+#******************************************************************************#
+
+ap = argparse.ArgumentParser()
+ap.add_argument("--src", required=True, help="source dir")
+ap.add_argument("--scalars", required=True, help="filepath/filename for scalers")
+ap.add_argument("--data", required=True, help="filepath/filename for dataset")
+args = vars(ap.parse_args())
+
+src_path = args["src"]
+scalers_path = args["scalars"]
+dataset_path = args["data"]
+
+filter_size = 91
+rows = 8000
+cols = 540
+
+files, _ = read_samples(src_path, ".mat")
+
+dset = []
+tmp_files = []
+for i in range(len(files)):
+    tmp = compute_data(files[i])
+    if (tmp.shape == (rows, cols)):
+        for j in range(cols):
+            tmp[:, j] = smooth(tmp[:, j], filter_size)
+        dset.append(tmp)
+        tmp_files.append(files[i])
+    else:
+        print("File dimension error | File:{} | Size:{}", files[i], tmp.shape)
+files = tmp_files
+
+dset = np.array(dset)
+
+fileObject = open(scalers_path,'rb')
+dict = pickle.load(fileObject)
+fileObject.close() 
+
+dset -= dict['means']
+dset -= dict['mins']
+dset /= (dict['maxs'] - dict['mins'])
+dset = dict['pca'].fit_transform(dset.reshape((dset.shape[0], -1)))
+
+hf = h5py.File(dataset_path, 'w')
+hf.create_dataset('X', data=dset)
+hf.create_dataset('y', data=files)
+hf.close()
+
+print("finished!!")
