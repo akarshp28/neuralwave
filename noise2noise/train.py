d4765e058febe910cffc369327043ea3ea5350d8
Kalvik <kalvik>
2018-12-08 21:48:19 -0500

    added command line arguments

--- noise2noise/train.py
+++ noise2noise/train.py
@@ -1,7 +1,6 @@
 import argparse
 import numpy as np
 from pathlib import Path
-from tensorflow import keras
 from tensorflow.keras.callbacks import LearningRateScheduler, ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import multi_gpu_model
@@ -24,15 +23,45 @@ class Schedule:
             return self.initial_lr * 0.25
         return self.initial_lr * 0.125
 
+def get_args():
+    parser = argparse.ArgumentParser(description="train noise2noise model",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--train_dir", type=str, required=True,
+                        help="train dir")
+    parser.add_argument("--test_dir", type=str, required=True,
+                        help="test dir")
+    parser.add_argument("--batch_size", type=int, default=16,
+                        help="batch size")
+    parser.add_argument("--nb_epochs", type=int, default=20,
+                        help="number of epochs")
+    parser.add_argument("--lr", type=float, default=0.001,
+                        help="learning rate")
+    parser.add_argument("--steps", type=int, default=1000,
+                        help="steps per epoch")
+    parser.add_argument("--loss", type=str, default="mse",
+                        help="loss; mse', 'mae', or 'l0' is expected")
+    parser.add_argument("--output_path", type=str, default="checkpoints",
+                        help="checkpoint dir")
+    parser.add_argument("--source_noise_model", type=str, default="gaussian,0,50",
+                        help="noise model for source")
+    parser.add_argument("--target_noise_model", type=str, default="clean",
+                        help="noise model for target")
+    parser.add_argument("--val_noise_model", type=str, default="gaussian,25,25",
+                        help="noise model for validation source")
+    args = parser.parse_args()
+
+    return args
+
 def main():
-    image_dir = "/home/kjakkala/neuralwave/data/CSI_preprocessed.h5"
-    test_dir = "/home/kjakkala/neuralwave/data/CSI_preprocessed.h5"
-    batch_size = 8
-    nb_epochs = 20
-    lr = 0.001
-    steps = 1000
-    loss_type = "mse"
-    output_path = "/home/kjakkala/neuralwave/data"
+    args = get_args()
+    train_dir = args.train_dir
+    test_dir = args.test_dir
+    batch_size = args.batch_size
+    nb_epochs = args.nb_epochs
+    lr = args.lr
+    steps = args.steps
+    loss_type = args.loss
+    output_path = Path(__file__).resolve().parent.joinpath(args.output_path)
     model = get_unet_model(1, 1, filters=[16, 16, 32], activation_func='relu', depth=4, inc_rate=2)
     model = multi_gpu_model(model, gpus=4)
 
@@ -46,12 +75,20 @@ def main():
         callbacks.append(UpdateAnnealingParameter(l0.gamma, nb_epochs, verbose=1))
         loss_type = l0()
 
+    output_path.mkdir(parents=True, exist_ok=True)
+
     model.compile(optimizer=opt, loss=loss_type, metrics=[PSNR])
-    source_noise_model = get_noise_model(source_noise_model)
-    target_noise_model = get_noise_model(target_noise_model)
-    generator = NoisyImageGenerator(image_dir, source_noise_model, target_noise_model, batch_size=batch_size)
-    val_generator = NoisyImageGenerator(test_dir, source_noise_model, target_noise_model, batch_size=batch_size)
+    source_noise_model = get_noise_model(args.source_noise_model)
+    target_noise_model = get_noise_model(args.target_noise_model)
+    generator = NoisyImageGenerator(train_dir, args.source_noise_model, args.target_noise_model, batch_size=batch_size)
+    val_generator = ValGenerator(test_dir, args.val_noise_model, args.target_noise_model, batch_size=batch_size)
     callbacks.append(LearningRateScheduler(schedule=Schedule(nb_epochs, lr)))
+    callbacks.append(ModelCheckpoint(str(output_path) + "/weights.{epoch:03d}-{val_loss:.3f}-{val_PSNR:.5f}.hdf5",
+                                 monitor="val_PSNR",
+                                 verbose=1,
+                                 mode="max",
+                                 save_best_only=True,
+                                 period=5))
 
     hist = model.fit_generator(generator=generator,
                                steps_per_epoch=steps,
@@ -61,7 +98,6 @@ def main():
                                callbacks=callbacks)
 
     np.savez(str(output_path.joinpath("history.npz")), history=hist.history)
-
 
 if __name__ == '__main__':
     main()
