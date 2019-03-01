3ce6376440a34cc82f0f5aaf1cf11b53a98940f6
kalvik <kd.kalvik@gmail.com>
2019-03-01 13:31:44 -0500

    removed old openset scripts, resnet scripts, updated current resnet and openset scripts

--- classifiers/resnet_500-270.py
+++ classifiers/resnet_500-270.py
@@ -5,44 +5,39 @@ from tensorflow.keras import optimizers
 from tensorflow.keras import layers
 import numpy as np
 import argparse
+import pickle
 import h5py
 
 def get_args():
     parser = argparse.ArgumentParser(description="train resent on CSI data",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--data_dir", type=str, required=True,
-                        help="h5py data file")
-    parser.add_argument("--lr", type=float, required=True, default="1e-3",
-                        help="initial learning rate")
-    parser.add_argument("--decay", type=float, required=True, default="1e-2",
-                        help="learning rate decay")
-    parser.add_argument("--nb_epochs", type=int, required=True, default="25",
-                        help="number of training epochs")
-    parser.add_argument("--num_classes", type=int, required=True, default="40",
-                        help="number of classee")
+    parser.add_argument("--data_dir", type=str, required=True, help="h5py data file")
+    parser.add_argument("--save_path", type=str, required=True, help="h5py data file for weight")
+    parser.add_argument("--history_path", type=str, required=True, help="pkl data file for history")
+    parser.add_argument("--ngpu", type=int, required=True, help="number of gpus")
     args = parser.parse_args()
 
     return args
 
 def main():
-    args = get_args()
-    lr=args.lr
-    epochs=args.nb_epochs
+    args=get_args()
     data_dir=args.data_dir
-    decay=args.decay
-    num_classes=args.num_classes
+    save_path=args.save_path
+    history_path=args.history_path
+    ngpu=args.ngpu
+
+    lr=1e-4
+    epochs=250
+    decay=1e-2
 
     hf = h5py.File(data_dir, 'r')
+    train_classes = np.array(hf.get('labels')).astype(str)
+    num_classes = len(train_classes)
     X_train = np.expand_dims(hf.get('X_train'), axis=-1)
     X_test = np.expand_dims(hf.get('X_test'), axis=-1)
     y_train = np.eye(num_classes)[hf.get('y_train')]
     y_test = np.eye(num_classes)[hf.get('y_test')]
     hf.close()
-
-    X_train = X_train[:, :, ::2]
-    X_test = X_test[:, :, ::2]
-
-    print(X_train.shape)
 
     input_layer = layers.Input(shape=(X_train.shape[1:]))
 
@@ -69,12 +64,17 @@ def main():
 
     model_base = Model(inputs=input_layer, outputs=x)
     model_base.summary()
-    model = multi_gpu_model(model_base, gpus=4)
+    model = multi_gpu_model(model_base, gpus=ngpu)
 
     model.compile(loss='categorical_crossentropy', optimizer=optimizers.Adam(lr=lr, decay=decay), metrics=['acc'])
-    history = model.fit(x=X_train, y=y_train, epochs=epochs, validation_data=(X_test, y_test), verbose=2)
+    history = model.fit(x=X_train, y=y_train, epochs=epochs, validation_data=(X_test, y_test), verbose=2, batch_size=16)
 
-    model_base.save("resnet.h5")
+    model_base.save(save_path)
+    
+    outfile = open(history_path,'wb')
+    pickle.dump(history.history, outfile)
+    outfile.close()
 
 if __name__ == '__main__':
     main()
+
