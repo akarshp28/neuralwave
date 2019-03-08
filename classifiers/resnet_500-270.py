d9187ad87fb4ef2f0696fb339785f9629880c63f
kalvik <kd.kalvik@gmail.com>
2019-03-08 16:42:17 -0500

    updated resnet with resblock v1

--- classifiers/resnet_500-270.py
+++ classifiers/resnet_500-270.py
@@ -1,4 +1,3 @@
-from model import identity_block, conv_block
 from tensorflow.keras.utils import multi_gpu_model
 from tensorflow.keras.models import Model
 from tensorflow.keras import optimizers
@@ -19,6 +18,46 @@ def get_args():
 
     return args
 
+def identity_block(input_tensor, filters, activation_func):
+    filters1, filters2 = filters
+    
+    x = layers.Conv2D(filters1, (1, 1), kernel_initializer='he_normal')(input_tensor)
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters1, (3, 3), padding='same', kernel_initializer='he_normal')(x)
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters2, (1, 1), kernel_initializer='he_normal')(x)
+    x = layers.BatchNormalization(axis=-1)(x)
+
+    x = layers.add([x, input_tensor])
+    x = layers.Activation('relu')(x)    
+    return x
+
+def conv_block(input_tensor, filters, activation_func, strides=(2, 2)):
+    filters1, filters2 = filters
+
+    x = layers.Conv2D(filters1, (1, 1), strides=strides, kernel_initializer='he_normal')(input_tensor)
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters1, (3, 3), padding='same', kernel_initializer='he_normal')(x)
+    x = layers.BatchNormalization(axis=-1)(x)
+    x = layers.Activation('relu')(x)
+
+    x = layers.Conv2D(filters2, (1, 1), kernel_initializer='he_normal')(x)
+    x = layers.BatchNormalization(axis=-1)(x)
+
+    shortcut = layers.Conv2D(filters2, (1, 1), strides=strides, kernel_initializer='he_normal')(input_tensor)
+    shortcut = layers.BatchNormalization(axis=-1)(shortcut)
+
+
+    x = layers.add([x, shortcut])
+    x = layers.Activation('relu')(x)
+    return x
+
 def main():
     args=get_args()
     data_dir=args.data_dir
@@ -26,8 +65,8 @@ def main():
     history_path=args.history_path
     ngpu=args.ngpu
 
-    lr=1e-4
-    epochs=250
+    lr=1e-3
+    epochs=100
     decay=1e-2
 
     hf = h5py.File(data_dir, 'r')
@@ -41,26 +80,25 @@ def main():
 
     input_layer = layers.Input(shape=(X_train.shape[1:]))
 
-    x = layers.Conv2D(64, (7, 7), strides=(2, 2))(input_layer)
-    x = layers.BatchNormalization(axis=-1)(x)
-    x = layers.Activation('relu')(x)
-    x = layers.MaxPooling2D((3, 3), strides=(2, 2))(x)
+	x = layers.Conv2D(16, (7, 7),
+	                  activation=None,
+	                  padding='same',
+	                  kernel_initializer='he_normal')(input_layer) 
+	x = layers.BatchNormalization(axis=-1)(x)
+	x = layers.Activation('relu')(x)
+	x = layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same")(x)
 
-    x = conv_block(x, [64, 256], "relu")
-    x = identity_block(x, [64, 256], "relu")
+	x = conv_block(x, [16, 64], "relu")
+	x = identity_block(x, [16, 64], "relu")
 
-    x = conv_block(x, [128, 512], "relu")
-    x = identity_block(x, [128, 512], "relu")
+	x = conv_block(x, [32, 128], "relu")
+	x = identity_block(x, [32, 128], "relu")
 
-    x = conv_block(x, [256, 1024], "relu")
-    x = identity_block(x, [256, 1024], "relu")
+	x = conv_block(x, [64, 128], "relu")
+	x = identity_block(x, [64, 128], "relu")
 
-    x = conv_block(x, [512, 2048], "relu")
-    x = identity_block(x, [512, 2048], "relu")
-
-    x = layers.GlobalAveragePooling2D()(x)
-    x = layers.Dense(num_classes, activation=None)(x)
-    x = layers.Activation('softmax')(x)
+	x = layers.GlobalAveragePooling2D()(x)
+	x = layers.Dense(num_classes, activation='softmax')(x)
 
     model_base = Model(inputs=input_layer, outputs=x)
     model_base.summary()
